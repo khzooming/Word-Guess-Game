340b310695dd98c6a13d7b31a966f7b271bb2240
Kathryn Helsley <kathrynhelsley@Kathryns-MacBook-Pro-2.local>
2019-03-15 09:54:27 -0700

    HW #3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,7 +14,9 @@ var oscarsGame = [
     "editing"
 ]
 
+// Need to get to the point of splitting out the array into letters.
 // Array.split(oscarsGame)
+// And maybe it's not necessary to join the letters back, I should be able to display them right next to each other when they are split apart.
 
 var score = 0;
 
@@ -26,39 +28,45 @@ runGame = function() {
     }
 }
 
+// Hide each letter from the word and then as they are guessed correctly, run an if else statement to unhide.
 
 // Keyboard letter press logic 
 document.onkeydown = function(event) {
-    var key_press = String.fromCharCode(event.keyCode);
-    var key_code = event.keyCode;
+    // var key_press = String.fromCharCode(event.keyCode);
+    var key = event.key;
     
     
-    document.getElementById("kp").innerHTML("#letterInputs") - key_press;
-    document.getElementById("kc").innerHTML - key_code;
+    document.getElementById("letterInputs").innerHTML = key;
+
+
+    // document.getElementById("kc").innerHTML - key_code;
     
     // I haven't gotten to the point of splitting apart the word into the individual letters, that should be inserted below instead of guessWord.
     letterGuess = function () {
-        if (key_press === guessWord);
+        if (key === guessWord);
         alert("Yeah!  You got a letter right!");    
         score++;
         }
     
     // I would go down all the letters A to Z 
-    if (key_press == "A") {
+    if (key == "A") {
         console.log("a was pressed");
         letterGuess();
 
-    }  else if (key_press == "B") {
+    }  else if (key == "B") {
         console.log("b was pressed");
         letterGuess();
-    }   else if (key_press == "C") {
+    }   else if (key == "C") {
         console.log("c was pressed");
         letterGuess();
-    }   else if (key_press == "D") {
+    }   else if (key == "D") {
         console.log("d was pressed");
         letterGuess();
     }
 }
+
+
+
 
 
 // runGame();
